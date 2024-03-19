0f8a958c2a527c1ae31f1fb3b334bacde5aba5f8
RkAirforce <rk.state.of.the.art@gmail.com>
2024-03-19 12:38:40 +0900

    Add: TailWind CSS追加

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,11 +8,16 @@ export default async function Home() {
   const todos = await getAllTodos()
 
   return (
-    <>
-      <AddTodo />
-      {todos && (
-        <TodoList todos={todos}/>
-      )}
-    </>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-grey-700 mt-32">To Do App</h1>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+          <AddTodo />
+          {todos && (
+            <TodoList todos={todos}/>
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
